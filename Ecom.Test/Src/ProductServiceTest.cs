f26223c9e5a4f4944a9837bd12f8035b4aec016a
hasanmd91 <hasanmd1991@hotmail.com>
2023-12-22 14:50:53 +0200

    build productService test

--- Ecom.Test/Src/ProductServiceTest.cs
+++ Ecom.Test/Src/ProductServiceTest.cs
@@ -68,6 +68,114 @@ namespace Ecom.Test.Src
             Assert.Equivalent(expected, response);
         }
 
+        [Theory]
+        [ClassData(typeof(SingleProductData))]
+        public async void GetOneProductByIdAsync_ShouldReturn_AProduct(Product? response, ProductReadDTO? result, Type? type)
+        {
+            var repo = new Mock<IProductRepo>();
+            repo.Setup(repo => repo.GetOneProductByIdAsync(It.IsAny<Guid>())).ReturnsAsync(response);
+            var productService = new ProductService(repo.Object, _mapper);
+
+            if (type is not null)
+            {
+                await Assert.ThrowsAsync(type, () => productService.GetOneProductByIdAsync(It.IsAny<Guid>()));
+            }
+            else
+            {
+                var expectedResult = await productService.GetOneProductByIdAsync(It.IsAny<Guid>());
+                Assert.Equivalent(result, expectedResult);
+            }
+        }
+
+        public class SingleProductData : TheoryData<Product?, ProductReadDTO?, Type?>
+        {
+            public SingleProductData()
+            {
+                Product product = new() { };
+                ProductReadDTO productReadDTO = _mapper.Map<Product, ProductReadDTO>(product);
+                Add(product, productReadDTO, null);
+                Add(null, productReadDTO, typeof(CustomException));
+            }
+        }
+
+        [Theory]
+        [ClassData(typeof(DeleteSingleProductData))]
+        public async void DeleteOneProductAsyncById_ShouldReturnValidResponse(Product? foundProduct, bool response, bool? result, Type? type)
+        {
+            var repo = new Mock<IProductRepo>();
+            if (foundProduct is not null)
+            {
+                repo.Setup(repo => repo.DeleteOneProductAsync(It.IsAny<Guid>())).ReturnsAsync(response);
+            }
+            else
+            {
+                repo.Setup(repo => repo.DeleteOneProductAsync(It.IsAny<Guid>())).ReturnsAsync(false);
+            }
+            var productService = new ProductService(repo.Object, _mapper);
+
+            if (type is not null)
+            {
+                await Assert.ThrowsAsync(type, () => productService.DeleteOneProductAsync(It.IsAny<Guid>()));
+            }
+            else
+            {
+                var expectedResult = await productService.DeleteOneProductAsync(It.IsAny<Guid>());
+                Assert.Equivalent(result, expectedResult);
+            }
+        }
+
+        public class DeleteSingleProductData : TheoryData<Product?, bool, bool?, Type?>
+        {
+            public DeleteSingleProductData()
+            {
+                Product product = new() { };
+                Add(product, true, true, null);
+                Add(null, false, false, typeof(CustomException));
+            }
+        }
+
+
+        [Theory]
+        [ClassData(typeof(CreateProductData))]
+        public async void CreateOneProductAsync_ShouldReturnValidResponse(Product product, ProductCreateDTO productCreateDTO, ProductReadDTO expected, Type? exceptionType)
+        {
+            var repo = new Mock<IProductRepo>();
+            repo.Setup(repo => repo.CreateOneProductAsync(It.IsAny<Product>())).ReturnsAsync(product);
+            var productService = new ProductService(repo.Object, _mapper);
+
+            if (exceptionType is not null)
+            {
+                await Assert.ThrowsAsync(exceptionType, () => productService.CreateOneProductAsync(productCreateDTO));
+            }
+            else
+            {
+                var response = await productService.CreateOneProductAsync(productCreateDTO);
+                Assert.Equivalent(expected, response);
+            }
+        }
+
+        public class CreateProductData : TheoryData<Product?, ProductCreateDTO, ProductReadDTO?, Type?>
+        {
+            public CreateProductData()
+            {
+                ProductCreateDTO productCreateDTO = new()
+                {
+                    Title = "tshirts",
+                    Description = "woevn",
+                    Price = 10,
+                    Quantity = 50,
+                    CategoryId = Guid.NewGuid(),
+                    Images = new List<Image> { new() { ImageUrl = "string" } }
+                };
+
+                Product product = _mapper.Map<ProductCreateDTO, Product>(productCreateDTO);
+                ProductReadDTO productReadDTO = _mapper.Map<Product, ProductReadDTO>(product);
+
+                Add(product, productCreateDTO, productReadDTO, null);
+                Add(null, productCreateDTO, null, typeof(CustomException));
+            }
+        }
+
 
     }
 }
